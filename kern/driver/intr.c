4d790092b8ef8b21e6a0c461821027f00b49922c
T202410611994671 <T202410611994671@eduxiji.net>
2024-12-24 10:32:17 +0000

    v6.3

--- kern/driver/intr.c
+++ kern/driver/intr.c
@@ -1,14 +1,19 @@
 #include <intr.h>
-
+  // LAB9 EXERCISE3: YOUR CODE
+//You should check the manual to complete the function
 /* intr_enable - enable irq interrupt */
 void
 intr_enable(void) {
-	__csrxchg(LISA_CSR_CRMD_IE, LISA_CSR_CRMD_IE, LISA_CSR_CRMD);
+	//__csrxchg(, , );
+	// 清除IE位以启用中断
+	__csrxchg(LISA_CSR_CRMD, LISA_CSR_CRMD_IE, 0);
 }
 
 /* intr_disable - disable irq interrupt */
 void
 intr_disable(void) {
-	__csrxchg(0, LISA_CSR_CRMD_IE, LISA_CSR_CRMD);
+	//__csrxchg(, , );
+	// 设置IE位以禁用中断
+	__csrxchg(LISA_CSR_CRMD, LISA_CSR_CRMD_IE, LISA_CSR_CRMD_IE);
 }
 
