eb64d54f9ccb8786ca1aed8a6fe1b1704ebe9acb
T202410611994671 <T202410611994671@eduxiji.net>
2024-12-24 06:18:15 +0000

    v1.4

--- kern/driver/console.c
+++ kern/driver/console.c
@@ -100,6 +100,8 @@ void serial_int_handler(void *opaque)
     int c = cons_getc();
 #if defined(LAB1_EX3) && defined(_SHOW_SERIAL_INPUT)
     // LAB1 EXERCISE3: YOUR CODE
+    kprintf("got input ");
+    kprintf("%c\n", c); // 假设kprintf是您的内核提供的打印函数
 #endif
 #ifdef LAB4_EX2
     extern void dev_stdin_write(char c);
