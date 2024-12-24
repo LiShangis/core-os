55abc40ebf52ad3fc6e456be557aaf9a322920d3
T202410611994671 <T202410611994671@eduxiji.net>
2024-12-24 11:19:23 +0000

    v7.2

--- kern/iotest/bio_request_test.c
+++ kern/iotest/bio_request_test.c
@@ -26,22 +26,43 @@ static struct bio* create_bio(int blockNum) {
     return newRequest;
 }
 
+// 计算从一个轨迹到另一个轨迹所需的时间
 static int calculate_track_to_track_time(int current_block, int next_block) {
 #ifdef LAB10_EX2
+	// 计算当前块和下一个块所在的轨迹号（磁道）
+	int current_track = current_block / (HPC * SPT); // HPC: 每磁道扇区数，SPT: 每磁道扇区数
+	int next_track = next_block / (HPC * SPT);
+	// 返回两个轨迹之间的距离乘以轨迹寻道时间
+	return ABS(next_track - current_track) * TRACK_SEEK_TIME; // 
     // LAB10 EXERCISE2: YOUR CODE
 #endif
 }
 
+// 计算从一个扇区到另一个扇区所需的时间
 static int calculate_sector_to_sector_time(int current_block, int next_block) {
 #ifdef LAB10_EX2
+	// 计算当前块和下一个块所在的扇区号
+	int current_sector = current_block % SPT; // SPT: 每磁道扇区数
+	int next_sector = next_block % SPT;
+	// 返回两个扇区之间的距离（加上一个磁道的扇区数后取余）乘以扇区寻道时间
+	return (next_sector - current_sector + SPT) % SPT * SECTOR_SEEK_TIME; 
     // LAB10 EXERCISE2: YOUR CODE
 #endif
 }
 
+// 根据遍历的块号数组计算总的磁盘I/O时间
 static int measure_time(int* traversed_blocks, int size, int begin_pos, int direction) {
 #ifdef LAB10_EX2
+	int total_time = 0; // 总时间
+	for(int i = 0; i < size; i++){
+		total_time += TIME_DATA_BLOCK_TRANSFER; // 每次块传输的时间
+		total_time += calculate_sector_to_sector_time(begin_pos, traversed_blocks[i]); // 扇区寻道时间
+		total_time += calculate_track_to_track_time(begin_pos, traversed_blocks[i]); // 轨迹寻道时间
+		begin_pos = traversed_blocks[i]; // 更新当前块号为遍历的块号
+	}
+	return total_time; // 返回总时间
     // LAB10 EXERCISE2: YOUR CODE
-    return 0;
+    //return 0;
 #endif
 }
 
