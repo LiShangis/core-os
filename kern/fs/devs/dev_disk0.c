90dcfc0423a96e5eed5bdd80fa9b6a105ef2f390
T202410611994671 <T202410611994671@eduxiji.net>
2024-12-24 09:03:18 +0000

    v6.2

--- kern/fs/devs/dev_disk0.c
+++ kern/fs/devs/dev_disk0.c
@@ -59,10 +59,10 @@ disk0_write_blks_nolock(uint32_t blkno, uint32_t nblks) {
 
 static int
 disk0_io(struct device *dev, struct iobuf *iob, bool write) {
-    off_t offset = iob->io_offset;
-    size_t resid = iob->io_resid;
-    uint32_t blkno = offset / DISK0_BLKSIZE;
-    uint32_t nblks = resid / DISK0_BLKSIZE;
+    off_t offset = iob->io_offset; // 从iobuf中获取I/O操作的起始偏移量
+    size_t resid = iob->io_resid;  // 从iobuf中获取剩余数据量
+    uint32_t blkno = offset / DISK0_BLKSIZE; // 计算起始块号
+    uint32_t nblks = resid / DISK0_BLKSIZE; // 计算需要传输的块数量
 
     /* don't allow I/O that isn't block-aligned */
     if ((offset % DISK0_BLKSIZE) != 0 || (resid % DISK0_BLKSIZE) != 0) {
@@ -75,32 +75,34 @@ disk0_io(struct device *dev, struct iobuf *iob, bool write) {
     }
 
     /* read/write nothing ? */
+    /* 如果没有数据需要读写，则直接返回 */
     if (nblks == 0) {
         return 0;
     }
 
-    lock_disk0();
-    while (resid != 0) {
-        size_t copied, alen = DISK0_BUFSIZE;
-        if (write) {
-            iobuf_move(iob, disk0_buffer, alen, 0, &copied);
-            assert(copied != 0 && copied <= resid && copied % DISK0_BLKSIZE == 0);
-            nblks = copied / DISK0_BLKSIZE;
-            disk0_write_blks_nolock(blkno, nblks);
+    lock_disk0(); // 锁定磁盘，确保操作的原子性和一致性
+    while (resid != 0) { // 当还有剩余数据需要处理时，继续循环
+        size_t copied, alen = DISK0_BUFSIZE; // 当还有剩余数据需要处理时，继续循环
+        if (write) { // 如果是写操作
+            iobuf_move(iob, disk0_buffer, alen, 0, &copied); // 从iobuf移动数据到磁盘缓冲区
+            assert(copied != 0 && copied <= resid && copied % DISK0_BLKSIZE == 0); // 确保复制的数据量有效
+            nblks = copied / DISK0_BLKSIZE; // 计算需要写入的块数
+            disk0_write_blks_nolock(blkno, nblks); // 写入数据到磁盘，不加锁版本
         }
-        else {
-            if (alen > resid) {
+        else { 
+            if (alen > resid) { // 如果请求的数据长度大于剩余数据，则调整请求长度
                 alen = resid;
             }
-            nblks = alen / DISK0_BLKSIZE;
-            disk0_read_blks_nolock(blkno, nblks);
-            iobuf_move(iob, disk0_buffer, alen, 1, &copied);
-            assert(copied == alen && copied % DISK0_BLKSIZE == 0);
+            nblks = alen / DISK0_BLKSIZE; // 计算需要读取的块数
+            disk0_read_blks_nolock(blkno, nblks); // 从磁盘读取数据，不加锁版本
+            iobuf_move(iob, disk0_buffer, alen, 1, &copied); // 从磁盘缓冲区移动数据到iobuf
+            assert(copied == alen && copied % DISK0_BLKSIZE == 0); // 确保复制的数据量与请求的一致
         }
+	// 减少剩余数据量  ; 更新块号
         resid -= copied, blkno += nblks;
     }
-    unlock_disk0();
-    return 0;
+    unlock_disk0(); // 解锁磁盘
+    return 0;  // 返回成功
 }
 
 static int
